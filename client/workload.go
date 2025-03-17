531c86975743319ac8e157b80d9f19fbfdc041ef
rahulk789 <rahul.u.india+1@gmail.com>
2025-03-17 22:25:06 +0530

    fix: url after update
    Signed-off-by: rahulk789 <rahul.u.india+1@gmail.com>

--- client/workload.go
+++ client/workload.go
@@ -23,7 +23,7 @@ func CreateWorkload(payload interface{}, cedanaURL string, cedanaAuthToken strin
 // GetClusterNodes makes a POST request to fetch nodes for a given cluster
 func DeleteWorkload(payload interface{}, cedanaURL string, cedanaAuthToken string) (string, error) {
 
-	resp, err := clientRequest("DELETE", cedanaURL+"/cluster/workload/job", cedanaAuthToken, payload)
+	resp, err := clientRequest("DELETE", cedanaURL+"/cluster/workload", cedanaAuthToken, payload)
 	if err != nil {
 		return "", fmt.Errorf("error decoding response: %v", err)
 	}
